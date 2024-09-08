027a00383ed6adba0993488b10982694c6957c77
LOW <126559845+ho8ae@users.noreply.github.com>
2024-09-08 16:14:16 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,38 @@
-from collections import deque
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-
-    
-def bfs(g,a,b):
-    n = len(g)
-    queue=deque()
-    queue.append((a,b))
-    g[a][b]=0
-    count = 1 #1이 몇 개 있는지 카운트
-    
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = x +dx[i]
-            ny = y + dy[i]
-            if nx<0 or nx>= n or ny<0 or ny>=n:
-                continue
-            if g[nx][ny] == 1:
-                g[nx][ny]=0
-                queue.append((nx,ny))
-                count += 1
-    return count
-    
 n = int(input())
-g =[]
+g = []
+num = []  # 리스트 선언 시 [] 필요
+
 for _ in range(n):
-    g.append(list(map(int,input())))
-cnt= []
+    g.append(list(map(int, input().strip())))  # 입력 처리
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def DFS(x, y):
+    if x < 0 or x >= n or y < 0 or y >= n:  # 범위 체크
+        return False
+    
+    if g[x][y] == 1:  # 조건 만족 시 DFS 진행
+        global count
+        count += 1
+        g[x][y] = 0  # 방문한 곳은 0으로 설정
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            DFS(nx, ny)
+        return True
+    return False
+
+count = 0
+total = 0
 for i in range(n):
     for j in range(n):
-        if g[i][j]==1:
-            cnt.append(bfs(g,i,j))
-
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+        if DFS(i, j) == True:
+            num.append(count)  # count 저장
+            total += 1
+            count = 0  # count 초기화
+ 
+num.sort()
+print(total)  # result 대신 total 출력
+for i in range(len(num)):
+    print(num[i])
