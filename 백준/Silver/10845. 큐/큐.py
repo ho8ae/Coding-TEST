62db8705ea35d99f5dbb7dc6b7dfd969ca083e67
LOW <126559845+ho8ae@users.noreply.github.com>
2025-01-06 17:26:02 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+#10828 #10845
+from collections import deque 
+import sys
+
+N = int(sys.stdin.readline())
+#이렇게 readline()으로 처리하면 시간초과를 피할 수 있음
+Q = deque()
+
+for i in range(N):
+    answer = sys.stdin.readline().split()
+    
+    if answer[0] == 'push':
+        Q.append(answer[1])
+    if answer[0] == 'front':
+        if Q:
+            print(Q[0])
+        else:
+            print(-1)
+    if answer[0] == 'back':
+        if Q:
+            print(Q[-1])
+        else:
+            print(-1)
+    if answer[0] == "pop":
+        if Q:
+            print(Q.popleft())
+        else:
+            print(-1)
+    if answer[0] == 'empty':
+        print(1 if not Q else 0)
+    if answer[0] == 'size':
+        print(len(Q))
+    
