adac2765358d80c4980ed9d3f81e5aed391d441b
LOW <126559845+ho8ae@users.noreply.github.com>
2025-01-16 16:23:21 +0900

    [Silver II] Title: 유기농 배추, Time: 208 ms, Memory: 33832 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,43 +1,42 @@
-# 1012 - 유기농 배추 BFS 버전
+# 유기농 배추 dfs 버전
 
-from collections import deque
+import sys
+sys.setrecursionlimit(5000)
 
-
-T = int(input()) # 몇 번의 테스트 케이스
-
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-
-
-def BFS(graph,x,y): #bfs 함수 구현
-    q = deque()
-    q.append([x,y])
-    graph[x][y]=0 # 방문 처리 1 -> 0 으로 만들기
+def dfs(x,y):
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
     
-    while q: # q가 비었다. 탐색 종료 모두 탐색
-        x,y=q.popleft()
-        for i in range(4): # 상하좌우 탐색
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0<=nx<M and 0<=ny<N and graph[nx][ny]==1: #배추밭 범위 지나지 않고 1인 경우
-                q.append([nx,ny])
-                graph[nx][ny]=0
+    # 경우의 수 탐색
+    
+    for i in range(4):
+        nx  = x +dx[i]
+        ny = y + dy[i]
+        
+        #범위를 벗어난 경우 pass
+        if nx<0 or ny<0 or nx>(n-1) or ny>(m-1): continue
+        
+        # 배추가 존재하는 경우 1인 경우
+        if graph[nx][ny] ==1:
+            graph[nx][ny] = 0
+            dfs(nx,ny)
 
+t= int(input())
+cnt = 0 
 
-for _ in range(T):
-    M,N,K = map(int,input().split())
-    graph = [[0]*(N) for _ in range(M)] # 빈 배추밭 만들기
+for _ in range(t):
+    m,n,k=map(int,input().split())
+    graph = [[0]*m for _ in range(n)]
+    for _ in range(k):
+        a, b= map(int,input().split())
+        graph[b][a] = 1
+        
+    cnt =0
     
-    for _ in range(K):
-        a,b = map(int,input().split())
-        graph[a][b] = 1
-    
-    cnt = 0
-    
-    for i in range(M):
-        for j in range(N):
-            if graph[i][j] == 1:
-                BFS(graph,i,j)
-                cnt +=1 
-    
+    for i in range(n): # 세로
+        for j in range(m): # 가로
+            if graph[i][j]==1:
+                graph[i][j] ==0
+                dfs(i,j)
+                cnt +=1
     print(cnt)
