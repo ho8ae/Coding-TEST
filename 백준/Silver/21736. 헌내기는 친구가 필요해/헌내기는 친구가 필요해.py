2816ccfe962a3eb1479b7fbc1c5744d2815ad3be
LOW <126559845+ho8ae@users.noreply.github.com>
2025-01-22 19:38:15 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 596 ms, Memory: 109556 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,34 +1,32 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6) # 재귀 제한 늘이기
+input = sys.stdin.readline
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-campus = [] # 캠퍼스 정보를 담는 리스트
-ix, iy = 0, 0 # 최초 시작 위치의 좌표 ix, iy
-answer = 0 # 만날 수 있는 사람의 수, 출력값
+def dfs(x,y):
+    global cnt
+    visited[x][y] = True
+    if graph[x][y] == 'P':
+        cnt+=1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+            if graph[nx][ny] !="X":
+                dfs(nx,ny)
 
-N, M = map(int, input().split()) # 캠퍼스 크기 입력
 
-for i in range(N):
-    campus.append(list(input()))
-    for j in range(M):
-        if campus[i][j] == 'I': # 시작 위치 찾기
-            ix, iy = i, j
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+cnt = 0
+n,m = map(int,input().split())
+graph = list(input() for _ in range(n))
+visited = [[False]*m for _ in range(n)]
 
-visited = [[0] * M for _ in range(N)] # 방문 정보를 담는 리스트
-
-deq = deque()
-deq.append([ix, iy]) # 최초 시작 위치 insert
-
-while deq: # 큐가 빌 때까지
-    x, y = deq.popleft()
-    for i in range(4):  # 상하좌우 탐색
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0: # 방문 안했으면 
-            visited[nx][ny] = 1 # 방문 표시
-            if campus[nx][ny] == 'O':  # 빈 공간 이면 
-                deq.append([nx, ny])
-            elif campus[nx][ny] == 'P':  # 사람 이면
-                deq.append([nx, ny])
-                answer += 1 # 만난 사람 수 증가
-
-print('TT' if answer == 0 else answer) # 정답 출력
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]=="I": # 시작지점 찾음
+            dfs(i,j)
+if cnt==0:
+    print("TT")
+else:
+    print(cnt)
