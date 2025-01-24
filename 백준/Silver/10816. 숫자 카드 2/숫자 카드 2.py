832dea8d56712d8e493c244401e4425b58cc21cd
LOW <126559845+ho8ae@users.noreply.github.com>
2025-01-24 22:26:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 672 ms, Memory: 115764 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,9 +1,15 @@
-from collections import Counter
-
 N = int(input())
-N_list = list(map(int, input().split()))
+N_list = list(map(int,input().split()))
 M = int(input())
-M_list = list(map(int, input().split()))
+M_list = list(map(int,input().split()))
 
-count = Counter(N_list)
-print(*(count[m] for m in M_list))
+# 미리 N_list를 순회하며 카운트
+count_dict = {}
+for num in N_list:
+    if num in count_dict:
+        count_dict[num] += 1
+    else:
+        count_dict[num] = 1
+
+# M_list의 각 숫자에 대해 카운트 출력
+print(*(count_dict.get(m, 0) for m in M_list))
