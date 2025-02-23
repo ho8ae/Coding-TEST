e2295eac35ecf5517b2b8690da38ba16d664a13b
LOW <126559845+ho8ae@users.noreply.github.com>
2025-02-23 17:18:18 +0900

    [level 2] Title: 가장 큰 수, Time: 1294.19 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,11 +1,10 @@
-import functools
-
-def compare(a,b):
-    t1 = str(a) + str(b)
-    t2 = str(b) + str(a)
-    return (int(t1)>int(t2)) -(int(t1) <int(t2))
 def solution(numbers):
-    sorted_nums= sorted(numbers,key = functools.cmp_to_key(lambda a, b:compare(a,b)),reverse=True )
+    numbers = list(map(str,numbers))
+    answer =''
+    lst = sorted(numbers,key=lambda x: x*3,reverse=True)
+
+    for i in lst:
+        answer += i
+
     
-    answer="".join(str(x) for x in sorted_nums)
-    return "0" if int(answer) == 0 else answer
+    return str(int(answer))
