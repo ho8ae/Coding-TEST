640634612d27b1ee97e002660727e7e64f049d5c
LOW <126559845+ho8ae@users.noreply.github.com>
2025-03-04 19:56:38 +0900

    [level 2] Title: 리코쳇 로봇, Time: 7.83 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,49 +1,51 @@
 from collections import deque
-
+# 방향 전환이 될떄 +1 
 def solution(board):
-    n, m = len(board), len(board[0])
+    answer = 0
+    n,m = len(board),len(board[0])
     
-    # 시작 위치와 목표 위치 찾기
-    start_x, start_y = 0, 0
+    # 초기 설정 시작 지점, deque, set을 이용 방향
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
-                start_x, start_y = i, j
+                startx,starty = i,j
+    # 상, 하, 좌, 우
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
-    # BFS 수행
-    queue = deque([(start_x, start_y, 0)])  # (x, y, 이동 횟수)
-    visited = set([(start_x, start_y)])  # 방문한 위치 저장
+    q = deque([(startx,starty,0)])  #x,y,카운트
+    v = set([(startx,starty)])
     
-    # 상하좌우 방향
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     
-    while queue:
-        x, y, count = queue.popleft()
+    
+    while q:
+        x,y,c = q.popleft() 
         
-        # 목표 위치에 도달했는지 확인
+        
+        # 종료 조건
         if board[x][y] == 'G':
-            return count
+            return c
         
-        # 네 방향으로 이동 시도
         for i in range(4):
-            nx, ny = x, y
-            
-            # 해당 방향으로 미끄러지기
+            nx = x
+            ny = y
             while True:
-                nx += dx[i]
-                ny += dy[i]
+                #빙판길 미끄러지기
+                nx = nx + dx[i]
+                ny = ny + dy[i]
                 
-                # 벽이나 장애물에 부딪히면 멈춤
-                if nx < 0 or nx >= n or ny < 0 or ny >= m or board[nx][ny] == 'D':
-                    nx -= dx[i]
-                    ny -= dy[i]
+                # 벽을 만나면 방향 전환
+                if  0 >nx or nx >= n or 0>ny or ny>=m or board[nx][ny] == 'D':
+                    nx = nx-dx[i]
+                    ny = ny-dy[i]
                     break
             
-            # 새로운 위치가 방문하지 않은 곳이라면 큐에 추가
-            if (nx, ny) not in visited:
-                visited.add((nx, ny))
-                queue.append((nx, ny, count + 1))
-    
-    # 목표 위치에 도달할 수 없는 경우
+            if(nx,ny) not in v:
+                v.add((nx,ny))
+                q.append((nx,ny,c+1))
+                
+                
+            
+            
+            
     return -1
