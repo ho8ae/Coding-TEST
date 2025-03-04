2ec2e42ccb30501f30990514deb0b4b6b560a28f
LOW <126559845+ho8ae@users.noreply.github.com>
2025-03-05 02:49:31 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 13.52 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,15 +1,14 @@
+import re
 def solution(s):
-    count = 0    # 변환 횟수
-    total_zero = 0    # 전체 제거된 0의 개수
     
-    while s != "1":
-        # 현재 문자열의 1의 개수를 세고, 0의 개수는 전체 길이에서 빼기
-        one_count = s.count("1")
-        zero_count = len(s) - one_count
-        total_zero += zero_count
-        
-        # 1의 개수를 이진수로 변환
-        s = bin(one_count)[2:]
+    count = 0
+    zero_count=0
+    while len(s) != 1:
+        zero_count += s.count('0')
+        s=re.sub(r'[^1]','',s)
+        s=bin(len(s))[2:]
         count += 1
+    
+    answer=[count,zero_count]
         
-    return [count, total_zero]
+    return answer
