b6804409a17429cbaf2999af85e1c8898e408e16
LOW <126559845+ho8ae@users.noreply.github.com>
2025-03-04 21:31:13 +0900

    [level 3] Title: 네트워크, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,16 @@
-def dfs(computers,visited,node):
-    visited[node] = True;
-    for idx,connected in enumerate(computers[node]):
-        if connected and not visited[idx]: # 연결되어 있으며 방문하지 않는 노드라면
-            dfs(computers,visited,idx)
-
+def dfs(computers,v,node):
+    v[node] = True
+    for idx,j in enumerate(computers[node]):
+        if j==1 and not v[idx]:
+            dfs(computers,v,idx)
 
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
-    for i in range(n):
-        if not visited[i]:
-            dfs(computers,visited,i)
-            answer+=1
+    v= [False] *n
+    
+    for i in range(len(v)):
+        if not v[i]:
+            dfs(computers,v,i)
+            answer += 1
+    
     return answer
