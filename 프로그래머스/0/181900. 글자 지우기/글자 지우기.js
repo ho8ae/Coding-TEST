f6eb92d38c6c6de98d951e2fb8101e9c42ef3dfb
LOW <126559845+ho8ae@users.noreply.github.com>
2025-06-20 21:16:23 +0900

    [level 0] Title: 글자 지우기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -0,0 +1,11 @@
+function solution(my_string, indices) {
+    var answer = '';
+    
+    for (let i = 0 ; i<indices.length; i++){
+        console.log(my_string[indices[i]].remove())
+       
+    }
+    
+    
+    return answer;
+}
