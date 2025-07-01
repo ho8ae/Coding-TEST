fd35a572696ed46dbaabb4b56e624552583d5b05
LOW <126559845+ho8ae@users.noreply.github.com>
2025-07-01 23:30:52 +0900

    [level 1] Title: 과일 장수, Time: 247.08 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,15 @@
+function solution(k, m, score) {
+    var answer = 0;
+    
+    score.sort((a, b) => b-a);   
+    
+    
+    let box = Math.floor(score.length / m);
+
+    
+    for(let i=1; i<=box; i++) {
+    
+        answer += score[m*i-1] * m;
+    }
+    return answer;
+}
