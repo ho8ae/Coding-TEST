6370f3eef9e0e6797410c840d3c2a9c6657d2eb1
LOW <126559845+ho8ae@users.noreply.github.com>
2025-07-10 23:27:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 5.64 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,13 +1,37 @@
+const numberDic = {
+    zero:'0',
+    one:1,
+    two:2,
+    three:3,
+    four:4,
+    five:5,
+    six:6,
+    seven:7,
+    eight:8,
+    nine:9
+}
 function solution(s) {
-    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = s;
-
-    for(let i = 0; i < numbers.length; i++) {
-        let arr = answer.split(numbers[i]);
-        console.log(arr)
-        answer = arr.join(i);
-        console.log(arr.join(i))
+    // 문자열 계속해서 저장하다가 맞으면 새 배열에 Push
+    let answer = []
+    let tempString = ''
+    for(let i =0; i<s.length; i++){
+        tempString += s[i]
+        console.log(tempString)
+        if(/\d/.test(s[i])){
+            answer.push(parseInt(s[i]))
+            tempString = '' // 초기화
+            continue;
+        }
+        if(numberDic[tempString] || numberDic[tempString] === '0'){
+            answer.push(numberDic[tempString]);
+            console.log(answer)
+            tempString = '' // 초기화
+            continue;
+        }
+     
     }
 
-    return Number(answer);
+    answer = parseInt(answer.join(''))
+    console.log("최종정답",answer)
+    return answer;
 }
